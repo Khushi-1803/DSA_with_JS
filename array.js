3a659187bf904ca88cc7e1848606397d0201cbc2
Khushi-1803 <khushi011504@gmail.com>
2026-03-13 14:26:32 +0530

    done foe 13-02-26

--- array.js
+++ array.js
@@ -41,4 +41,50 @@ let arr3 = [function () { }, new Object(), [], {},
 
 console.log(countDatatypes(arr3));
 
+// 5️⃣How to remove falsy values from an array in JavaScript ?
+
+let arr4 = [1,,4,null,89,undefined," ",0];
+
+let newArr = [];
+
+arr4.forEach((num) => {
+    if (
+        num !== false &&
+        num !== 0 &&
+        num !== " " &&
+        num !== 0n &&
+        num !== null &&
+        num !== undefined &&
+        !Number.isNaN(num)
+    ) {
+        newArr.push(num);
+    }
+});
+
+console.log(newArr);
+
+//6️⃣Finding largest value of an array
+let arr5 = [11,22,87,89,67,43,62]
+let lar = arr5[0]
+for (let i = 1; i < arr5.length; i++) {
+    if (arr5[i]>lar) {
+    lar = arr5[i]
+}
+    
+}
+console.log(lar);
+
+//7️⃣Finding second largest value of an array
+let arr6 = [11,22,87,89,67,43,62]
+let largest = arr6[0]
+let secLargest = arr6[1]
+for (let i = 2; i < arr6.length; i++) {
+    if (arr6[i]>largest) {
+     secLargest = largest   
+     largest = arr6[i]
+}
+}
+console.log(secLargest);
+
+
 
